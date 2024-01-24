d8f86968f685bf09449460965de2c7d6b8e3492f
Athira Mangalath Muraleedharan <Amangalathmural5484@conestogac.on.ca>
2024-01-24 18:19:45 -0500

    Initial assignment commit

--- UIAssignment1/ViewController.swift
+++ UIAssignment1/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var numberHolder = 0
+    var behaviourValue = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var resultHolder: UILabel!
+    @IBAction func minusButton(_ sender: Any) {
+        numberHolder -= behaviourValue
+        resultHolder.text=String(numberHolder)
+    }
+    
+    @IBAction func plusButton(_ sender: Any) {
+        numberHolder += behaviourValue
+        resultHolder.text=String(numberHolder)
+    }
+    
+    @IBAction func incrementBy2(_ sender: Any) {
+        behaviourValue = 2
+    }
+    
+    @IBAction func resetBehaviorValue(_ sender: Any) {
+        behaviourValue = 1
+        numberHolder = 0
+        resultHolder.text=String(numberHolder)
+    }
 }
 
